f2cf56ff9d44fe9ef6bc335a717438187f4a80e6
Danny de Wit <danny@veltenmedia.nl>
2014-05-24 23:27:54 +0200

    Improved fetcher (made it object oriented)

--- clientside/static/js/app/workers/fetcher.js
+++ clientside/static/js/app/workers/fetcher.js
@@ -7,81 +7,85 @@ console.log('NOW RUNNING :: worker/fetcher.js');
 var previousDataset;
 var intervalCheck;
 
-// Simplified XMLHttpRequest load-function. Shamelessly copied from 
-// source: http://techslides.com/html5-web-workers-for-ajax-requests/
-// editted for better implementation and use.
-function load(url, callback) {
-    var xhr;
-     
-    if(typeof XMLHttpRequest !== 'undefined') xhr = new XMLHttpRequest();
-    else {
-        var versions = ["MSXML2.XmlHttp.5.0", 
-                        "MSXML2.XmlHttp.4.0",
-                        "MSXML2.XmlHttp.3.0", 
-                        "MSXML2.XmlHttp.2.0",
-                        "Microsoft.XmlHttp"]
-
-         for(var i = 0, len = versions.length; i < len; i++) {
-            try {
-                xhr = new ActiveXObject(versions[i]);
-                break;
-            }
-            catch(e){}
-         } // end for
-    }
-    xhr.onreadystatechange = ensureReadiness;
-    
-    function ensureReadiness() {
-        if(xhr.readyState < 4) {
-            return;
+// SKOV FetcherWorker
+var SKOV = SKOV || {};
+SKOV.fetcherWorker = {
+    messageEventHandler: function(e){
+        switch(e.data){
+            case 'dataCheck':
+                console.log('WORKER :: Message received and understood: data check executing.');
+                SKOV.fetcherWorker.dataCheck();
+            break;
+            default:
+                console.log('WORKER :: Message NOT understood.')
         }
-        if(xhr.readyState === 4) {
-            callback(xhr);
-        }           
-    }
-    xhr.open('GET', url, true);
-    xhr.send('');
-}
+    },
 
-// Do datacheck request
-function dataCheck(){
-    // Log event
-    console.log('WORKER :: Datacheck :: Started.');
-    console.time('WORKER :: Datacheck :: Timer');
-    load('//dannydewit.nl/cmda/srp/serverside/fetcher.php', function(data){
-        if(data !== previousDataset){
-            if(data.readyState === 4 || data.status === 200){
-                console.log('WORKER :: Datacheck :: New dataset retrieved!');
-                try{
-                    previousDataset = data;
-                    self.postMessage(data.response);
+    // Simplified XMLHttpRequest load-function. Shamelessly copied from 
+    // source: http://techslides.com/html5-web-workers-for-ajax-requests/
+    // editted for better implementation and use.
+    load: function(url, callback) {
+        var xhr;
+         
+        if(typeof XMLHttpRequest !== 'undefined') xhr = new XMLHttpRequest();
+        else {
+            var versions = ["MSXML2.XmlHttp.5.0", 
+                            "MSXML2.XmlHttp.4.0",
+                            "MSXML2.XmlHttp.3.0", 
+                            "MSXML2.XmlHttp.2.0",
+                            "Microsoft.XmlHttp"]
+
+             for(var i = 0, len = versions.length; i < len; i++) {
+                try {
+                    xhr = new ActiveXObject(versions[i]);
+                    break;
                 }
-                catch(e){
-                    console.log('EXCEPTION :: Caught: ',e);
+                catch(e){}
+             } // end for
+        }
+        xhr.onreadystatechange = ensureReadiness;
+        
+        function ensureReadiness() {
+            if(xhr.readyState < 4) {
+                return;
+            }
+            if(xhr.readyState === 4) {
+                callback(xhr);
+            }           
+        }
+        xhr.open('GET', url, true);
+        xhr.send('');
+    },
+
+    // Do datacheck request
+    dataCheck: function(){
+        // Log event
+        console.log('WORKER :: Datacheck :: Started.');
+        console.time('WORKER :: Datacheck :: Timer');
+        SKOV.fetcherWorker.load('//dannydewit.nl/cmda/srp/serverside/fetcher.php', function(data){
+            if(data !== previousDataset){
+                if(data.readyState === 4 || data.status === 200){
+                    console.log('WORKER :: Datacheck :: New dataset retrieved!');
+                    try{
+                        previousDataset = data;
+                        self.postMessage(data.response);
+                    }
+                    catch(e){
+                        console.log('EXCEPTION :: Caught: ',e);
+                    }
                 }
+                else{
+                    console.error('WORKER :: Datacheck :: New dataset found, but an error in the readystate and/or status occured. Aborting.');
+                }
+
+                
             }
             else{
-                console.error('WORKER :: Datacheck :: New dataset found, but an error in the readystate and/or status occured. Aborting.');
+                console.log('WORKER :: Datacheck :: No new dataset found.');
             }
-
-            
-        }
-        else{
-            console.log('WORKER :: Datacheck :: No new dataset found.');
-        }
-    });
+        });
+    },
 };
 
-
-
 // LISTEN TO WORKER COMMANDS FROM PARENT
-self.addEventListener('message', function(e) {
-    switch(e.data){
-        case 'dataCheck':
-            console.log('WORKER :: Message received and understood: data check executing.');
-            dataCheck();
-        break;
-        default:
-            console.log('WORKER :: Message NOT understood.')
-    }
-}, false);
+self.addEventListener('message', SKOV.fetcherWorker.messageEventHandler, false);
